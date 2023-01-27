e36e1c6e81e506e8f3b2b331dfcc6a41f0bbe222
penrosed <24257802+penrosed@users.noreply.github.com>
2023-01-27 00:11:15 +0000

    Initial Commit! All pre-github work.
    This project was started outside of GitHub. However, after drafting the mechanics, I thought it'd be handy to have the project on GitHub!
    
    To summarise:
    
    • In this game, the player (the 'Phrog') jumps from left to right, platform to platform, collecting coins! The Phrog then gets to spend those coins on phresh phroggy phashion!
    • In my implementation, the player is ENTIRELY STATIONARY on the x axis, to avoid floating point errors later on! So platforms move from right to left under the player.
    
    Here's what I've implemented so far:
    
    • The 'PlatformSpawner' GameObject spawns pre-designed groups of platforms, called 'sections'.
    • These 'sections', once spawned, move from right to left whenever the player is in the air,
    and destroy themselves when they go too far offscreen.
    • The player can jump by clicking/tapping and dragging, then letting go.
    • The player clicking/tapping and dragging is taken as a percentage of screen height. This is then run through an animation curve, allowing small drags to evaluate to 0f (no jump), and for higher drags to have diminishing returns.
    • The player has an increased gravity scale when they fall, making jumps feel snappier. This 'fallingGravity' can be adjusted in the editor.
    • When the player clicks/taps and drags, an arc is displayed. This is the trajectory the player would take if the mouse/finger was released. (Very difficult to find a solution that worked with the different gravity scales and stationary x axis. Ended up adapting https://austin-mackrell.medium.com/unity-trajectory-prediction-simulation-method-5b441ee1604 )
    
    Not every solution I've implemented is perfect. Still a work in progress!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [Space]
+    public float _speed;
+    public float _fallGravity = 1.5f;
+    [SerializeField] private float _maxJumpForce = 600;
+    [SerializeField] private AnimationCurve _swipeCurve;
+    [HideInInspector] public float _originalGravity;
+    [HideInInspector] public float _distance;
+    
+    [Space] // The layer that contains all collidable surfaces.
+    [SerializeField] private LayerMask _jumpableSurfaces;
+
+    // The start and end vectors for when we click & drag, and the
+    // force applied when we jump.
+    private Vector2 _dragStart, _dragEnd;
+    private Vector2 _jumpForce;
+
+    // References to our physics components & trajectory manager.
+    private BoxCollider2D _boxCollider;
+    private Rigidbody2D _rigidbody2D;
+    private TrajectoryManager _trajectoryManager;
+
+    // Awake is called before any other calls
+    private void Awake()
+    {
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _trajectoryManager = GameObject.FindObjectOfType<TrajectoryManager>();
+        _originalGravity = _rigidbody2D.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTouchData();                  // Find out if we've dragged along the touchscreen, and by how much
+
+        if (IsGrounded())                   // Find out if we're on the ground. If we are...
+        {                                   // {
+            
+            GetJumpForce();                 //    Update the amount of force we'll apply when we jump, based on touch input.
+            DrawPredictedTrajectory();      //    Based on that force, get our Trajectory manager to draw a predicted trajectory line.
+                                            //
+            if (Input.GetMouseButtonUp(0))  //    If we've released our finger...
+            {                               //    {
+                Jump();                     //       JUMP!
+                _jumpForce = Vector2.zero;  //       Reset our jump force.
+            }                               //    }
+        }                                   // }
+
+        else                                      // Otherwise (if we're in the air)...
+        {                                         // {
+            _distance += _speed * Time.deltaTime; //    Increase our distance travelled.
+        }                                         // }
+
+        if (_rigidbody2D.velocity.y < 0)              // If we're falling...
+        {                                             // {
+            _rigidbody2D.gravityScale = _fallGravity; //    Switch to our falling gravity scale.
+        }                                             // }
+    }
+
+    // Gets the start and end position when we click and drag. Called every frame.
+    private void UpdateTouchData()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _dragStart = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            _dragEnd = Input.mousePosition;
+        }
+    }
+
+    // Get the trajectory manager to draw where it thinks we'll jump.
+    private void DrawPredictedTrajectory()
+    {
+        if (Input.GetMouseButtonDown(0)) // When we first tap...
+        {
+            _trajectoryManager.InitSimScene();   // Create a simulated scene with all platforms for our Traj Manager.
+            _trajectoryManager.EnableLine(true); // Enable the Trajectory Manager's line renderer.
+        }
+        if (Input.GetMouseButton(0)) // While we're tapping & holding...
+        {
+            // Given our jump force, ask the Trajectory manager to draw a predicted arc.
+            _trajectoryManager.SimulateHop(this.transform, _jumpForce, _speed, _originalGravity, _fallGravity);
+        }
+        if (Input.GetMouseButtonUp(0)) // When we release our finger...
+        {
+            _trajectoryManager.EnableLine(false); // Hide the trajectory line.
+        }
+    }
+
+    // Given the click and drag data, calculates how much force to apply to the player.
+    private void GetJumpForce()
+    {
+        // Find the length of our click & drag (in pixels on the screen)
+        float dragScreenPercent = Mathf.Abs((_dragEnd - _dragStart).magnitude) / Screen.height;
+
+        // By running that number through our swipe curve, find out how
+        // much of the maximum jump force we're applying (0 to 100%)
+        float amountOfForce = _swipeCurve.Evaluate(dragScreenPercent);
+
+        _jumpForce = new(0, amountOfForce * _maxJumpForce); // Apply that % to our max jump force.
+    }
+
+    // JUMP!
+    private void Jump()
+    {
+        _rigidbody2D.AddForce(_jumpForce);
+    }
+
+    // Check if the player is grounded using a BoxCast. If we are, reset the gravity scale.
+    public bool IsGrounded()
+    {
+        // This BoxCast will only trigger when it hits an object on the layer designated in 'jumpableSurfaces'.
+        bool grounded = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f, Vector2.down, .1f, _jumpableSurfaces);
+        _rigidbody2D.gravityScale = (grounded) ? _originalGravity : _rigidbody2D.gravityScale;
+        return grounded;
+    }
+}
